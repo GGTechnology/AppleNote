3379677a837c1aea9708e4de4f2f3fb73ffca8c0
mingcol <>
2019-07-17 18:10:43 +0800

    no message

---
+++ Demo/视频拍摄/视频拍摄/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视频拍摄
+//
+//  Created by mingcol on 2019/7/17.
+//  Copyright © 2019 mingcol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/视频拍摄/视频拍摄/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视频拍摄
+//
+//  Created by mingcol on 2019/7/17.
+//  Copyright © 2019 mingcol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
