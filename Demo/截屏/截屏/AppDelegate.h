cd56003e97033fd0f47000d8fc4920cf50a412da
kadisVmei <huzhengyangjob@qq.com>
2019-08-12 11:50:29 +0800

    Log：CaptureScreen Funtion

---
+++ Demo/截屏/截屏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  截屏
+//
+//  Created by kadis on 2019/8/12.
+//  Copyright © 2019 kadis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
