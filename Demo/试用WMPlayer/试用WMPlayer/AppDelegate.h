c2b205d7de32e5b9e43869da4e3bc866b7e97be0
kadisVmei <lvshitianxia@outlook.com>
2019-08-15 09:25:28 +0800

    Log：WMPlayer

---
+++ Demo/试用WMPlayer/试用WMPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  试用WMPlayer
+//
+//  Created by kadis on 2019/8/15.
+//  Copyright © 2019 https://github.com/GGTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
