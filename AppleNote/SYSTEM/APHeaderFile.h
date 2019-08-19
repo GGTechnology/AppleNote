aa998c7481cc46ed0bd7cf7a82b0980361b60629
kadisVmei <lvshitianxia@outlook.com>
2019-08-19 19:12:35 +0800

    no message

---
+++ AppleNote/SYSTEM/APHeaderFile.h
@@ -0,0 +1,15 @@
+//
+//  APHeaderFile.h
+//  AppleNote
+//
+//  Created by kadis on 2019/8/16.
+//  Copyright © 2019 mingcol. All rights reserved.
+//
+
+#ifndef APHeaderFile_h
+#define APHeaderFile_h
+
+#import <YTKKeyValueStore.h>
+
+
+#endif /* APHeaderFile_h */
--- AppleNote/ViewController.swift
+++ AppleNote/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        self.view.backgroundColor = UIColor.red
+        
+        self.view.backgroundColor = UIColor.lightGray
     }
 }
 
