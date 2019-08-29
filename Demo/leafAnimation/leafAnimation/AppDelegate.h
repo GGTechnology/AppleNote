b37f2c01d3238af3f8c77b04217403d02949ba57
kadisVmei <lvshitianxia@outlook.com>
2019-08-29 19:52:24 +0800

    no message

---
+++ Demo/leafAnimation/leafAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  leafAnimation
+//
+//  Created by kadis on 2019/8/29.
+//  Copyright © 2019 https://github.com/GGTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
