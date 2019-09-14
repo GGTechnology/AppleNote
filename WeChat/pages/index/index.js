3aa733dc903719babbc1dace52e930edad32d80b
kadisVmei <lvshitianxia@outlook.com>
2019-09-14 16:08:33 +0800

    no message

--- WeChat/pages/index/index.js
+++ WeChat/pages/index/index.js
@@ -1,15 +1,72 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+// var num = 0
 Page({
   data: {
     time: (new Date()).toString(),
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    // 条件逻辑
+    length:4,
+    condition:true,
+
+    // 列表渲染
+    listArray: [{
+      message: 'one',
+    }, {
+      message: 'two'
+    }, {
+      message: 'three'
+    }],
+    objectArray: [
+      { id: 5, unique: 'unique_5' },
+      { id: 4, unique: 'unique_4' },
+      { id: 3, unique: 'unique_3' },
+      { id: 2, unique: 'unique_2' },
+      { id: 1, unique: 'unique_1' },
+      { id: 0, unique: 'unique_0' },
+    ],
+    numberArray: [1, 2, 3, 4],
+
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-06-18'
+    }
   },
+
+  // 列表渲染
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(e) {
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
