264d535c33a9b0f3de2b8276c011a96cf766cdf5
kadisVmei <lvshitianxia@outlook.com>
2019-09-24 20:17:44 +0800

    no message

---
+++ AppleNote/Common/Extension/Refresh.swift
@@ -0,0 +1,17 @@
+//
+//  Refresh.swift
+//  AppleNote
+//
+//  Created by kadis on 2019/9/24.
+//  Copyright © 2019 mingcol. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIScrollView {
+    
+    
+    
+    
+}
--- AppleNote/Common/TableView/LGDRefreshFooterView.swift
+++ AppleNote/Common/TableView/LGDRefreshFooterView.swift
@@ -28,14 +28,77 @@ var img: UIImageView?
 // 加载「菊花」
 var refreshView: UIActivityIndicatorView?
 
+
 class LGDRefreshFooterView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.cyan
+        creatViews()
+        self.addObserver(self, forKeyPath: "type", options: .new, context: nil)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func creatViews() {
+        if (title == nil) {
+            title = UILabel.init(frame: CGRect(x: SW/3, y: 0, width: SW/3, height: 50))
+            self.addSubview(title!)
+            
+        }
+        if (img == nil) {
+            img = UIImageView.init(frame: CGRect(x: SW/3-30, y: 10, width: 30, height: 30))
+            img?.image = UIImage.init(named: "jw_refresh")
+            self.addSubview(img!)
+        }
+        if (refreshView == nil) {
+            refreshView = UIActivityIndicatorView.init(style: .gray)
+            refreshView?.center = CGPoint(x: SW/3-15, y: 25)
+            refreshView?.isHidden = true
+            refreshView?.startAnimating()
+            self.addSubview(refreshView!)
+        }
+        title?.font = UIFont.systemFont(ofSize: 18)
+        title?.text = cancelLoad
+        title?.textAlignment = .center
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "type") {
+            title?.text = getRefreshStatus(status: type!)
+            if (type! == .willLoad) {
+                img?.isHidden = false
+                refreshView?.isHidden = true
+                UIView.animate(withDuration: 0.3, animations: {
+                    img?.transform = CGAffineTransform(rotationAngle: .pi)
+                })
+                UIView.animate(withDuration: 0.3) {
+                    img?.transform = CGAffineTransform(rotationAngle: .pi)
+                }
+            } else if (type! == .loading) {
+                img?.isHidden = true
+                refreshView?.isHidden = false
+            } else {
+                img?.isHidden = false
+                refreshView?.isHidden = true
+                UIView.animate(withDuration: 0.3) {
+                    img?.transform = CGAffineTransform(rotationAngle: 0)
+                }
+            }
+        }
+    }
+    
+    func getRefreshStatus(status:LGD_LoadShowType) -> String {
+        switch status {
+        case .willLoad:
+            return willLoad
+        case .loading:
+            return loading
+        case .cancelLoad:
+            return cancelLoad
+        default:
+            break
+        }
+    }
 }
