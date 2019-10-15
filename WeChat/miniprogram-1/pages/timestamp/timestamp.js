d70342cb386bc51f298dc410489e29695f1115a6
GGTechnology <lvshitianxia@outlook.com>
2019-10-16 00:42:00 +0800

    第一次推送微信小程序

---
+++ WeChat/miniprogram-1/pages/timestamp/timestamp.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    // 数据渲染
+    message: '我在 timestamp.js 文件',
+
+    // 列表渲染
+    array: ['遍历数组方法', 1, 2, 3, 4, 5],
+
+    // 条件渲染
+    view: 'MINA',
+
+    // 模板
+    staffA: { firstName: '🍎', lastName: '🍏' },
+    staffB: { firstName: '😄', lastName: '😢' },
+    staffC: { firstName: '🏀', lastName: '🎾' }
+  }
+})
