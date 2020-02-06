8910ff2ab1a305375d16f861cb333efaec8edb53
GGTechnology <lvshitianxia@outlook.com>
2020-02-06 16:12:14 +0800

    no message

---
+++ Demo/点击屏幕事件touchesBegan等/点击屏幕时间touchesBegan等/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  点击屏幕时间touchesBegan等
+//
+//  Created by LvGuangDa on 2020/2/6.
+//  Copyright © 2020 shijunnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Demo/点击屏幕事件touchesBegan等/点击屏幕时间touchesBegan等/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  点击屏幕时间touchesBegan等
+//
+//  Created by LvGuangDa on 2020/2/6.
+//  Copyright © 2020 shijunnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
