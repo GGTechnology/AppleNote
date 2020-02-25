31c748a3d3b2d5484a97c87d4b5e6556a00c82f5
kadisVmei <lvshitianxia@outlook.com>
2020-02-25 20:40:40 +0800

    no message

---
+++ Demo/打印NSURL属性/打印NSURL属性/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  打印NSURL属性
+//
+//  Created by kadis on 2020/2/25.
+//  Copyright © 2020 https://github.com/GGTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
