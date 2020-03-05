9d1d4de0fad3ed5af24276ab79cab8034e35697e
GGTechnology <lvshitianxia@outlook.com>
2020-03-05 20:42:37 +0800

    no message

--- AppleNote/View/APBaseTableView.swift
+++ AppleNote/View/APBaseTableView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class APBaseTableView: UITableView {
+class APBaseTableView: APTableView {
 
     
     
--- AppleNote/View/APNoteTableView.swift
+++ AppleNote/View/APNoteTableView.swift
@@ -8,6 +8,6 @@
 
 import UIKit
 
-class APNoteTableView: APBaseTableView {
+class APNoteTableView: APTableView {
     
 }
