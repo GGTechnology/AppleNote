bc1b0b62d305999fafd064126d60f25e4de75eff
kadisVmei <lvshitianxia@outlook.com>
2020-08-15 18:13:37 +0800

    no message

---
+++ Demo/WMplayer/WMplayerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMplayerTest
+//
+//  Created by kadis on 2020/6/19.
+//  Copyright © 2020 lvguangda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@property (copy, nonatomic) NSArray *sidArray;
+@property (copy, nonatomic) NSArray *videoArray;
+
+@end
+
