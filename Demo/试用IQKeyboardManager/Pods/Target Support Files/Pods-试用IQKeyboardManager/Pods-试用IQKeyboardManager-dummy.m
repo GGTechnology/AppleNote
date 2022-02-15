b24197838c4f10ebf5c4701f4cb081be446b0d50
apple <lvshitianxia@outlook.com>
2022-02-15 11:27:14 +0800

    Delete Demo directory

--- Demo/试用IQKeyboardManager/Pods/Target Support Files/Pods-试用IQKeyboardManager/Pods-试用IQKeyboardManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods___IQKeyboardManager : NSObject
-@end
-@implementation PodsDummy_Pods___IQKeyboardManager
-@end
--- Demo/试用WMPlayer/Pods/Target Support Files/Pods-试用WMPlayer/Pods-试用WMPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods___WMPlayer : NSObject
-@end
-@implementation PodsDummy_Pods___WMPlayer
-@end
